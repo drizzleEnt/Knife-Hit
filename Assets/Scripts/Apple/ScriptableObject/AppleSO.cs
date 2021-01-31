da20a697247c018db05e9662effccc83d6ac0a86
drizzleEnt <70242740+drizzleEnt@users.noreply.github.com>
2021-01-31 20:11:06 +0300

    init

---
+++ Assets/Scripts/Apple/ScriptableObject/AppleSO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new appleSO", menuName = "Apple", order = 51)]
+public class AppleSO : ScriptableObject
+{
+    [SerializeField] private float _chance;
+
+    public float Chacne => _chance;
+}
---
+++ Assets/Scripts/Vibration/Vibrator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vibrator : MonoBehaviour
+{
+    [SerializeField] private KnifeSpawner _spawner;
+
+    private void OnEnable()
+    {
+        _spawner.Lose += OnVibration;
+        _spawner.KnifeInLog += OnVibration;
+        _spawner.AllKnifesInTarget += OnVibration;
+    }
+
+    private void OnDisable()
+    {
+        _spawner.Lose -= OnVibration;
+        _spawner.KnifeInLog -= OnVibration;
+        _spawner.AllKnifesInTarget -= OnVibration;
+    }
+
+    private void OnVibration()
+    {
+        Handheld.Vibrate();
+    }
+}
